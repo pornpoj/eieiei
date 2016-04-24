1ab0adb77d469eb4a0caa65ca60c37744e58893d
pornpoj ketkaew <pornpoj@pornpojs-MacBook-Air.local>
2016-04-24 14:23:10 +0700

    sendtext

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var token = "CAAYLBZA5g0wkBADVVtC38ZACAJI4vvruIuG8c1bE1NFcZAdl6QZBjIB00xZCoQbuUkbOVYCf6ff3brdVruZC4BHhWLcPoe5yfYIqqFmdT8sEnM53iJiSgN7dX98eQnAKWTCVDj0LxfZCZAZBpt7cQEICYBICuhEWWbnZBkhdecAOnChV9hKiw9sMsZBmnGGTdZC5JP86rVoEo2E1NQZDZD";
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -29,6 +30,27 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200);
 });
+
+function sendTextMessage(sender, text) {
+  var messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 app.listen(app.get('port'), function () {
   console.log('Example aoo listening on port ' + app.get('port') + ' !')
