4e56df554022554a4b02a8061ade44b4507f3578
pornpoj ketkaew <pornpoj@pornpojs-MacBook-Air.local>
2016-04-24 14:08:43 +0700

    test

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ app.get('/webhook/', function (req, res) {
 
 app.set('port', (process.env.PORT || 5000))
 
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+      console.log(text);
+    }
+  }
+  res.sendStatus(200);
+});
+
 app.listen(app.get('port'), function () {
   console.log('Example aoo listening on port ' + app.get('port') + ' !')
 })
